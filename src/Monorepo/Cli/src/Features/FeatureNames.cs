9ad1b329dcfa72970357d076397881614bad7236
Ales Kobr <kobr.ales@hotmail.cz>
2022-02-24 01:18:56 +0100

    refactor: move features const list

---
+++ src/Monorepo/Cli/src/Features/FeatureNames.cs
@@ -0,0 +1,13 @@
+namespace _42.Monorepo.Cli.Features
+{
+    public static class FeatureNames
+    {
+        public const string Packages = "packages";
+        public const string GitVersion = "git-version";
+        public const string Stylecop = "stylecop";
+
+        public const string Tests = "tests";
+        public const string TestsXunit = "tests-xunit";
+        public const string TestsNunit = "tests-nunit";
+    }
+}
