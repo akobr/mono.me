11f23e19a84409eeaf11e455d8a376b93e6a7bb0
Ales Kobr <kobr.ales@hotmail.cz>
2022-02-24 01:25:33 +0100

    feat(alpha): extend command new version

--- src/Monorepo/Cli/src/Commands/New/NewVersionCommand.cs
+++ src/Monorepo/Cli/src/Commands/New/NewVersionCommand.cs
@@ -3,22 +3,32 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using _42.Monorepo.Cli.ConventionalCommits;
+using _42.Monorepo.Cli.Configuration;
 using _42.Monorepo.Cli.Extensions;
+using _42.Monorepo.Cli.Git;
 using _42.Monorepo.Cli.Output;
 using LibGit2Sharp;
 using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Options;
 using Semver;
 
 namespace _42.Monorepo.Cli.Commands.New
 {
-    [Command("version")]
+    [Command("version", Description = "Set new version.")]
     public class NewVersionCommand : BaseCommand
     {
-        public NewVersionCommand(IExtendedConsole console, ICommandContext context)
+        private readonly IGitHistoryService _historyService;
+        private readonly ReleaseOptions _options;
+
+        public NewVersionCommand(
+            IExtendedConsole console,
+            ICommandContext context,
+            IGitHistoryService historyService,
+            IOptions<ReleaseOptions> configuration)
             : base(console, context)
         {
-            // no operation
+            _historyService = historyService;
+            _options = configuration.Value;
         }
 
         [Argument(0, Description = "A custom version to set.")]
@@ -26,7 +36,6 @@ namespace _42.Monorepo.Cli.Commands.New
 
         protected override async Task<int> ExecuteAsync()
         {
-
             using var repo = new Repository(Context.Repository.Record.Path);
             SemVersion currentVersion = await Context.Item.TryGetDefinedVersionAsync() ?? new SemVersion(0, 1);
             var versionFile = await Context.Item.TryGetVersionFilePathAsync() ?? Path.Combine(Context.Repository.Record.Path, Constants.VERSION_FILE_NAME);
@@ -38,66 +47,89 @@ namespace _42.Monorepo.Cli.Commands.New
 
             if (lastChangeInVersion == null)
             {
-                throw new InvalidOperationException("There is no version history in the repository.");
+                Console.WriteImportant("There is no version history in the current location of the mono-repository.");
+                return ExitCodes.WARNING_NO_WORK_NEEDED;
             }
 
-            Stack<Commit> affectingCommits = new();
+            var report = _historyService.GetHistory(Context.Item.Record.RepoRelativePath, new Commit[] { lastChangeInVersion.Commit });
+            var hasChanges = report.Changes.Count > 0;
 
-            foreach (var commit in repo.Head.Commits)
+            if (report.Changes.Count < 1 && report.UnknownChanges.Count < 1)
             {
-                if (commit.Id == lastChangeInVersion.Commit.Id)
-                {
-                    break;
-                }
-
-                affectingCommits.Push(commit);
+                Console.WriteImportant($"There are no changes in current version '{currentVersion}'.");
+                return ExitCodes.WARNING_NO_WORK_NEEDED;
             }
 
-            if (affectingCommits.Count < 1)
-            {
-                throw new InvalidOperationException($"There are no changes in current version '{currentVersion}'.");
-            }
+            var majorChangeTypeSet = new HashSet<string>(_options.Changes.Major);
+            var minorChangeTypeSet = new HashSet<string>(_options.Changes.Minor);
+            var patchChangeTypeSet = new HashSet<string>(_options.Changes.Patch);
+            var harmlessChangeTypeSet = new HashSet<string>(_options.Changes.Harmless);
 
-            List<IConventionalMessage> changes = new(affectingCommits.Count);
-            StrictConventionalParser parser = new();
+            var majorChanges = report.Changes.Where(ch => ch.Message.IsBreakingChange || majorChangeTypeSet.Contains(ch.Message.Type)).ToList();
+            var minorChanges = report.Changes.Where(ch => minorChangeTypeSet.Contains(ch.Message.Type)).ToList();
+            var pathChanges = report.Changes.Where(ch => patchChangeTypeSet.Contains(ch.Message.Type)).ToList();
+            var harmlessChanges = report.Changes.Where(ch => harmlessChangeTypeSet.Contains(ch.Message.Type)).ToList();
 
-            while (affectingCommits.Count > 0)
-            {
-                var commit = affectingCommits.Pop();
-                if (parser.TryParseCommitMessage(commit.MessageShort, out var message))
-                {
-                    changes.Add(message!);
-                }
-            }
+            var hasMajorChange = majorChanges.Count > 0;
+            var hasMinorChange = minorChanges.Count > 0;
+            var hasPatchChange = pathChanges.Count > 0;
 
-            var majorChange = changes.Any(m => m.IsBreakingChange);
-            var minorChange = changes.Any(m => m.Type == "feat");
-            var patchChange = changes.Any(m => m.Type != "chore" || m.Type != "test" || m.Type != "build");
+            var newVersion = currentVersion.Change(prerelease: string.Empty);
 
-            if (majorChange)
+            if (hasMajorChange)
             {
-                currentVersion = new(currentVersion.Major + 1);
+                newVersion = new(currentVersion.Major + 1);
             }
-            else if (minorChange)
+            else if (hasMinorChange)
             {
-                currentVersion = new(currentVersion.Major, currentVersion.Minor + 1);
+                newVersion = new(currentVersion.Major, currentVersion.Minor + 1);
             }
-            else if (patchChange)
+            else if (hasPatchChange)
             {
-                currentVersion = new(currentVersion.Major, currentVersion.Minor, currentVersion.Patch + 1);
+                newVersion = new(currentVersion.Major, currentVersion.Minor, currentVersion.Patch + 1);
             }
             else
             {
-                throw new InvalidOperationException("There are no changes which should affect the version.");
+                Console.WriteImportant("There are no changes which should affect the version.");
             }
 
-            Colorful.Console.WriteLine($"New version should be {currentVersion}");
-            Colorful.Console.WriteLine();
-            Colorful.Console.WriteLine("Changes:");
-
-            foreach (var change in changes)
+            if (hasChanges)
             {
-                Console.WriteLine($" > {change.Type}: {change.Description}");
+                Console.WriteImportant($"New version should be ", $"{newVersion}".ThemedHighlight(Console.Theme));
+                Console.WriteLine();
+                Console.WriteHeader("Changes:");
+                var count = 0;
+
+                foreach (var change in report.Changes)
+                {
+                    count++;
+                    Console.WriteLine($"> {change.Message.GetFullRepresentation()}");
+
+                    if (count >= 50)
+                    {
+                        Console.WriteLine("... and more".ThemedLowlight(Console.Theme));
+                        break;
+                    }
+                }
+            }
+
+            if (report.UnknownChanges.Count > 0)
+            {
+                Console.WriteLine();
+                Console.WriteHeader("Unknown changes:");
+                var count = 0;
+
+                foreach (var unknownCommit in report.UnknownChanges)
+                {
+                    count++;
+                    Console.WriteLine($"> {unknownCommit.MessageShort}");
+
+                    if (count >= 50)
+                    {
+                        Console.WriteLine("... and more".ThemedLowlight(Console.Theme));
+                        break;
+                    }
+                }
             }
 
             return ExitCodes.SUCCESS;
