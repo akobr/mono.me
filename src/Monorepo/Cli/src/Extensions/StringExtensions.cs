8592950200d376aad2411ab6cfad988237b72464
Ales Kobr <kobr.ales@hotmail.cz>
2022-02-24 01:25:26 +0100

    fix: add function to validate item name

--- src/Monorepo/Cli/src/Extensions/StringExtensions.cs
+++ src/Monorepo/Cli/src/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace _42.Monorepo.Cli.Extensions
 {
@@ -13,5 +14,39 @@ namespace _42.Monorepo.Cli.Extensions
         {
             return text.Equals(value, StringComparison.Ordinal);
         }
+
+        // TODO: [P3] Each section needs to be a valid identifier, starts with letter or _ (not only the first one)
+        public static string ToValidItemName(this string? text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return string.Empty;
+            }
+
+            var builder = new StringBuilder(text.Trim());
+
+            for (var i = 0; i < builder.Length; i++)
+            {
+                var character = builder[i];
+
+                if (char.IsLetterOrDigit(character)
+                    || character == '_'
+                    || character == '.')
+                {
+                    continue;
+                }
+
+                builder[i] = '_';
+            }
+
+            if (builder.Length > 0
+                && char.IsDigit(builder[0]))
+            {
+                builder.Insert(0, '_');
+            }
+
+            return builder.ToString();
+        }
+
     }
 }
