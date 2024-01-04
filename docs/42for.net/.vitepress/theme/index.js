aa99a95b432a53d285f0dd1e1fca41fc1ce3c5cc
Ales Kobr <kobr.ales@outlook.com>
2024-01-04 17:01:00 +0100

    docs: update home layout and add reference

---
+++ docs/42for.net/.vitepress/theme/index.js
@@ -0,0 +1,8 @@
+import DefaultTheme from 'vitepress/theme'
+import _42Layout from './_42Layout.vue'
+import './_42styles.css'
+
+export default {
+    extends: DefaultTheme,
+    Layout: _42Layout
+  }
