37a373e055e49e380e72cc690e858954616c5b05
Ales Kobr <kobr.ales@outlook.com>
2024-02-02 03:03:24 +0100

    feat: add GetResponsibilities endpoint

--- src/Platform/Storyteller/Api.Functions/src/V1/AnnotationsHttp.cs
+++ src/Platform/Storyteller/Api.Functions/src/V1/AnnotationsHttp.cs
@@ -2,6 +2,7 @@ using System.Net;
 using _42.Platform.Storyteller.Api.Models;
 using _42.Platform.Storyteller.Api.OpenApi;
 using _42.Platform.Storyteller.Api.Security;
+using _42.Platform.Storyteller.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -34,14 +35,13 @@ public class AnnotationsHttp
     [OpenApiResponseWithBody(HttpStatusCode.InternalServerError, Definitions.ContentTypes.Json, typeof(ErrorResponse), Description = Definitions.Descriptions.ResponseInternalServerError)]
     public async Task<IActionResult> GetAnnotations(
         [HttpTrigger(AuthorizationLevel.Anonymous, Definitions.Methods.Get, Route = Definitions.Routes.Annotations.V1.Annotations)]
-        Continuation token,
         HttpRequestData request,
-        string organization = Constants.DefaultTenantName,
-        string project = Constants.DefaultProjectName,
-        string view = Constants.DefaultViewName)
+        string organization,
+        string project,
+        string view,
+        [FromQuery] string? continuationToken = null)
     {
         request.CheckScope(Scopes.Annotation.Read, Scopes.Annotation.Write, Scopes.Default.Read, Scopes.Default.Write);
-
         var dataRequest = new Request
         {
             Types = new[]
@@ -52,7 +52,7 @@ public class AnnotationsHttp
                 AnnotationType.Context,
                 AnnotationType.Execution,
             },
-            ContinuationToken = token.ContinuationToken,
+            ContinuationToken = continuationToken,
             Organization = organization,
             Project = project,
             View = view,
@@ -60,5 +60,55 @@ public class AnnotationsHttp
 
         var response = await _annotations.GetAnnotationsAsync(dataRequest);
         return new OkObjectResult(response);
+    }
+
+    [Function(nameof(GetResponsibilities))]
+    [OpenApiOperation(Definitions.Routes.Annotations.V1.Responsibilities, Definitions.Tags.Annotations)]
+    [OpenApiSecurity(Definitions.SecuritySchemas.Manual, SecuritySchemeType.Http, Scheme = OpenApiSecuritySchemeType.Bearer, BearerFormat = Definitions.Others.JWT, Description = Definitions.Descriptions.SecureManual)]
+    [OpenApiSecurity(Definitions.SecuritySchemas.Integrated, SecuritySchemeType.OAuth2, Flows = typeof(OAuthFlows))]
+    [OpenApiParameter(Definitions.Parameters.Organization, In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = Definitions.Descriptions.Organization)]
+    [OpenApiParameter(Definitions.Parameters.Project, In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = Definitions.Descriptions.Project)]
+    [OpenApiParameter(Definitions.Parameters.View, In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = Definitions.Descriptions.View)]
+    [OpenApiParameter(Definitions.Parameters.NameQuery, In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = Definitions.Descriptions.NameQuery)]
+    [OpenApiParameter(Definitions.Parameters.ContinuationToken, In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = Definitions.Descriptions.ContinuationToken)]
+    [OpenApiResponseWithBody(HttpStatusCode.OK, Definitions.ContentTypes.Json, typeof(Response), Description = "The list of responsibilities.")]
+    [OpenApiResponseWithBody(HttpStatusCode.BadRequest, Definitions.ContentTypes.Json, typeof(ErrorResponse), Description = Definitions.Descriptions.ResponseBadRequest)]
+    [OpenApiResponseWithoutBody(HttpStatusCode.Unauthorized, Description = Definitions.Descriptions.ResponseUnauthorized + $"{Scopes.Annotation.Read}, {Scopes.Annotation.Write}, {Scopes.Default.Read}, {Scopes.Default.Write}")]
+    [OpenApiResponseWithBody(HttpStatusCode.InternalServerError, Definitions.ContentTypes.Json, typeof(ErrorResponse), Description = Definitions.Descriptions.ResponseInternalServerError)]
+    public async Task<IActionResult> GetResponsibilities(
+        [HttpTrigger(AuthorizationLevel.Anonymous, Definitions.Methods.Get, Route = Definitions.Routes.Annotations.V1.Responsibilities)]
+        HttpRequestData request,
+        string organization,
+        string project,
+        string view,
+        [FromQuery] string? nameQuery = null,
+        [FromQuery] string? continuationToken = null)
+    {
+        request.CheckScope(Scopes.Annotation.Read, Scopes.Annotation.Write, Scopes.Default.Read, Scopes.Default.Write);
+        var dataRequest = new Request
+        {
+            Types = new[]
+            {
+                AnnotationType.Responsibility,
+            },
+            ContinuationToken = continuationToken,
+            Organization = organization,
+            Project = project,
+            View = view,
+        };
+
+        if (!string.IsNullOrWhiteSpace(nameQuery))
+        {
+            dataRequest.Conditions = new[]
+            {
+                new Request.Condition<Responsibility>
+                {
+                    Predicate = r => r.Name.Contains(nameQuery),
+                },
+            };
+        }
+
+        var response = await _annotations.GetAnnotationsAsync(dataRequest);
+        return new OkObjectResult(response.AsTyped<Responsibility>());
     }
 }
