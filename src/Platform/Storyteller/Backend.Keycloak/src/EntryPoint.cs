74805eeb96ce6b20da184250e3b3fe165563821a
Ales Kobr <kobr.ales@outlook.com>
2026-02-25 01:25:50 +0100

    feat: add basic m2m auth by clients in Keycloak

---
+++ src/Platform/Storyteller/Backend.Keycloak/src/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using _42.Platform.Storyteller.Accessing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace _42.Platform.Storyteller;
+
+public static class EntryPoint
+{
+    public static IServiceCollection AddKeycloakMachineAccess(
+        this IServiceCollection services)
+    {
+        services.AddHttpClient();
+        services.AddSingleton<IMachineAccessService, KeycloakMachineAccessService>();
+        return services;
+    }
+}
