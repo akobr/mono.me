2f10591fe34e512638128a4c1a8cc9a304799787
Ales Kobr <ales.kobr@bricknode.com>
2026-03-19 00:25:40 +0100

    feat(platform): add nice JSON rendering in CLI

--- src/Platform/Cli/src/Output/ExtendedConsoleExtensions.cs
+++ src/Platform/Cli/src/Output/ExtendedConsoleExtensions.cs
@@ -1,10 +1,15 @@
 using System;
+using System.IO;
 using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
 using _42.CLI.Toolkit.Output;
 using _42.Platform.Cli.Output.Exceptions;
 using _42.Platform.Storyteller;
 using Newtonsoft.Json;
 using Sharprompt;
+using Spectre.Console;
+using Spectre.Console.Json;
 
 namespace _42.Platform.Cli.Output;
 
@@ -12,11 +17,35 @@ public static class ExtendedConsoleExtensions
 {
     public static void WriteJson(this IExtendedConsole @this, object data)
     {
-        using var jWriter = new JsonTextWriter(@this.Console.Out);
-        jWriter.CloseOutput = false;
-        var serializer = JsonSerializer.CreateDefault();
-        serializer.Serialize(jWriter, data);
+        var sb = new StringBuilder();
+        using var sw = new StringWriter(sb);
+        using var jWriter = new JsonTextWriter(sw) { Formatting = Formatting.Indented, CloseOutput = false };
+        JsonSerializer.CreateDefault().Serialize(jWriter, data);
+
+        var json = sb.ToString();
+
+        // Render JsonText to a capture buffer so we can prepend line numbers.
+        var captureWriter = new StringWriter();
+        var captureConsole = AnsiConsole.Create(new AnsiConsoleSettings
+        {
+            Out = new AnsiConsoleOutput(captureWriter),
+            ColorSystem = ColorSystemSupport.TrueColor,
+            Ansi = AnsiSupport.Yes,
+        });
+        captureConsole.Write(new JsonText(json));
+
+        var lines = captureWriter.ToString()
+            .TrimEnd('\r', '\n')
+            .Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+        var numWidth = lines.Length.ToString().Length;
+
         Console.WriteLine();
+        foreach (var (line, index) in lines.Select((l, i) => (l, i)))
+        {
+            AnsiConsole.Markup($"[grey]{(index + 1).ToString().PadLeft(numWidth)}[/] [dim]│[/] ");
+            Console.Write(line);
+            Console.WriteLine();
+        }
     }
 
     public static void WriteJsonToFile(this IExtendedConsole @this, object data, string filePath, IFileSystem fileSystem)
