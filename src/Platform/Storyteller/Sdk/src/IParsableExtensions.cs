4b499244bdbc2d87dc9a49e645830399840b1f83
Ales Kobr <kobr.ales@outlook.com>
2026-03-27 00:32:06 +0100

    feat: add token retrieval and storage to storyteller sdk

---
+++ src/Platform/Storyteller/Sdk/src/IParsableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Kiota.Abstractions.Serialization;
+
+namespace ApiSdk;
+
+public static class IParsableExtensions
+{
+    public static TOutput? ToTyped<TInput, TOutput>(this TInput source, JsonSerializerOptions? options = null)
+        where TInput : IParsable
+    {
+        var json = KiotaJsonSerializer.SerializeAsString(source);
+        return JsonSerializer.Deserialize<TOutput>(json, options);
+    }
+
+    public static async Task<T?> ToTypedAsync<T>(this IParsable source, JsonSerializerOptions? options = null)
+    {
+        var json = await KiotaJsonSerializer.SerializeAsStringAsync(source);
+        return JsonSerializer.Deserialize<T>(json, options);
+    }
+}
